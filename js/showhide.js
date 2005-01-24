c8b99096679d5989f93f603fe2104788d8684d0c
akid <akid@a8256b1f-a225-0410-95af-91ab0afbf006>
2005-01-24 02:14:31 +0000

    for v140
    git-svn-id: http://svn.sourceforge.jp/svnroot/p2-php/p2_1x/trunk@145 a8256b1f-a225-0410-95af-91ab0afbf006

--- js/showhide.js
+++ js/showhide.js
@@ -1,27 +1,32 @@
 /* p2 - メニューカテゴリの開閉のためのJavaScript */
 
-var gHideClass = 'itas_hide';
+var gHideClassName = 'itas_hide';
 
 if (document.getElementById) {
 	document.writeln('<style type="text/css" media="all">');
 	document.writeln('<!--');
-	document.writeln('.' + gHideClass + '{ display:none; }');
+	document.writeln('.' + gHideClassName + '{ display:none; }');
 	document.writeln('-->');
 	document.writeln('</style>');
 }
 
 function showHide(id) {
+	
 	var obj = document.getElementById(id);
+		
+	if (!obj) {
+		return false;
+	}
 
 	if (obj.style.display == 'block') {
 		obj.style.display = "none";
 	} else if(obj.style.display == 'none') {
 		obj.style.display = "block";
 	} else {
-		if (obj.className == gHideClass) {
-			obj.style.display = "block";
-		} else {
+		if (obj.className != gHideClassName) {
 			obj.style.display = "none";
+		} else {
+			obj.style.display = "block";
 		}
 	}
 	return false;
