769076ef565e65b42c0b9fe4d77c0791103feb11
unpush <unpush@unpush.net>
2008-03-23 02:37:26 +0900

    HTMLポップアップをクリックでするように
    $conf_user_def['iframe_popup']にオプション追加

--- lib/showthreadpc.class.php
+++ lib/showthreadpc.class.php
@@ -809,7 +809,14 @@ EOP;
 
         // リンクの属性にHTMLポップアップ用のイベントハンドラを加える
         $pop_attr = $attr;
-        $pop_attr .= " onmouseover=\"showHtmlPopUp('{$pop_url}',event,{$_conf['iframe_popup_delay']})\"";
+        if ($_conf['iframe_popup'] == 4) {
+            if (is_null($mode)) {
+                $mode = 2;
+            }
+            $pop_attr .= " onClick=\"showHtmlPopUp('{$pop_url}',event,0); return false;\"";
+        } else {
+            $pop_attr .= " onmouseover=\"showHtmlPopUp('{$pop_url}',event,{$_conf['iframe_popup_delay']})\"";
+        }
         $pop_attr .= " onmouseout=\"offHtmlPopUp()\"";
 
         // 最終調整
