3a22a85518ca42f8ee7386869210d8c5c6abb6ef
akid <akid@a8256b1f-a225-0410-95af-91ab0afbf006>
2008-12-07 15:29:40 +0000

    old skinname "conf_user_style"
    git-svn-id: http://svn.sourceforge.jp/svnroot/p2-php/p2/trunk@555 a8256b1f-a225-0410-95af-91ab0afbf006

--- lib/P2Util.php
+++ lib/P2Util.php
@@ -21,7 +21,12 @@ class P2Util
             return null;
         }
         if (file_exists($_conf['skin_setting_path'])) {
-            return rtrim(file_get_contents($_conf['skin_setting_path']));
+            $skin = rtrim(file_get_contents($_conf['skin_setting_path']));
+            // 2008/12/08 データ形式が古い場合や拡張版データを考慮して
+            if ($skin == 'conf_user_style') {
+                return null;
+            }
+            return $skin;
         }
         return null;
     }
