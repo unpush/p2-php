972d9c5938119c1d4813c3207907b04bf92427a4
rsk <rsk@a8256b1f-a225-0410-95af-91ab0afbf006>
2009-02-10 15:43:45 +0000

    expack: - Divided IP configurations from HostCheck.php   into the individual files.
    git-svn-id: http://svn.sourceforge.jp/svnroot/p2-php/p2ex/trunk@578 a8256b1f-a225-0410-95af-91ab0afbf006

---
+++ conf/ip_emobile.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = '/\\.pool\\.e(?:mnet|-?mobile)\\.ne\\.jp$/';
+
+$band = array(
+    '117.55.1.224/27',
+);
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: cp932
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+// vim: set syn=php fenc=cp932 ai et ts=4 sw=4 sts=4 fdm=marker:
