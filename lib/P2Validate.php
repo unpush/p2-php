e190199ddea66e4694e4355defd8650f15ed92a7
akid <akid@a8256b1f-a225-0410-95af-91ab0afbf006>
2009-06-05 10:08:58 +0000

    ログイン管理から●を設定しようとした時、メアドに+が含まれていると受け付けられなかったのを修正した。
    git-svn-id: http://svn.sourceforge.jp/svnroot/p2-php/p2/trunk@702 a8256b1f-a225-0410-95af-91ab0afbf006

--- lib/P2Validate.php
+++ lib/P2Validate.php
@@ -9,79 +9,81 @@ class P2Validate
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function host($str)
     {
-        if (!preg_match('{^[\\w\\-./:@~]+$}', $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        if (preg_match('{^[\\w\\-./:@~]+$}', $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
     
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function bbs($str)
     {
-        if (!preg_match('{^[\\w\\-]+$}', $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        if (preg_match('{^[\\w\\-]+$}', $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
     
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function key($str)
     {
-        if (!preg_match('{^[\\w]+$}', $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        if (preg_match('{^[\\w]+$}', $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
     
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function spmode($str)
     {
-        if (!preg_match('{^[\\w]+$}', $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        if (preg_match('{^[\\w]+$}', $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
     
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function mail($str)
     {
-        if (!preg_match("/^[_a-z0-9-]+([._a-z0-9-]+)@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,})$/i", $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        // ゆるい判定
+        $mstr = 'a-z0-9@?!#%&`+*^{}_$\\/\\-';
+        if (preg_match("/^[.$mstr]+@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,})$/i", $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
     
     /**
      * @static
      * @access  public
-     * @return  string|null  不正ならエラーメッセージを返す
+     * @return  null|string  不正ならエラーメッセージを返す
      */
     function login2chPW($str)
     {
         // 正確な許可文字列は不明
-        if (!preg_match('~^[\\w.,@:/+-]+$~', $str)) {
-            return sprintf('validation error: %s', __FUNCTION__);
+        if (preg_match('~^[\\w.,@:/+-]+$~', $str)) {
+            return null;
         }
-        return null;
+        return sprintf('validation error: %s', __FUNCTION__);
     }
 }
