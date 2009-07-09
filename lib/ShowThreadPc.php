31c686b0b8f96cc57eaf80e7e8449eda6cf4de1c
unpush <unpush@gmail.com>
2009-07-09 22:22:04 +0900

    colored ID 最新版(from jiyuwiki.com)
    http://jiyuwiki.com/index.php?cmd=read&page=rep2%A4%C7%A3%C9%A3%C4%A4%CE%C7%D8%B7%CA%BF%A7%CA%D1%B9%B9&word=rep2

--- lib/ShowThreadPc.php
+++ lib/ShowThreadPc.php
@@ -767,7 +767,7 @@ EOP;
     }
 
     /**
-     * marged from http://jiyuwiki.com/index.php?cmd=read&page=rep2%A4%C7%A3%C9%A3%C4%A4%CE%C7%D8%B7%CA%BF%A7%CA%D1%B9%B9&alias%5B%5D=pukiwiki%B4%D8%CF%A2
+     * Merged from http://jiyuwiki.com/index.php?cmd=read&page=rep2%A4%C7%A3%C9%A3%C4%A4%CE%C7%D8%B7%CA%BF%A7%CA%D1%B9%B9&alias%5B%5D=pukiwiki%B4%D8%CF%A2
      *
      * @access  private
      * @return  string
@@ -775,48 +775,171 @@ EOP;
     function coloredIdStr($idstr, $id)
     {
         global $STYLE;
-        
-        // [$id] >= 2　ココの数字でスレに何個以上同じＩＤが出た時に背景色を変えるか決まる
-        if (isset($this->thread->idcount[$id]) && $this->thread->idcount[$id] < 2) {
+
+        // Version.20081215
+        //   ID本体とは別に、ID:の部分に別の背景色を適用
+        //   色変換処理をcolorchange.phpにまとめた。
+        //   その他、こまごまとした修正
+        // Version.20081216
+        //   HSV,HLSに加え、L*C*h表色系にも対応（ライブラリも修正）
+        // Version.20081224 設定で変換前と変換後のカラーコードを表示できるようにした（デバッグ用？）
+        // Version.20081228 色パラメータ設定を色空間別に個別化
+        require_once P2_LIB_DIR . '/colorchange.php';
+
+        if (!(isset($this->thread->idcount[$id]) && $this->thread->idcount[$id] >= 2)) {
+            //[$id] >= 2　ココの数字でスレに何個以上同じＩＤが出た時に背景色を変えるか決まる
             return $idstr;
         }
+        if (isset($this->id2idstr[$id])) return $this->id2idstr[$id];
+
+        // IDから色の元を抽出
+        $coldiv=12; // 色相環の分割数
         $raw = base64_decode(substr($id, 0, 8));
+        $arr = unpack('v', substr($raw, 0, 4));
+        $color=$arr[1] % $coldiv;
+        $color2=(2+$color+($arr[1] >> 4) % ($coldiv-4)) % $coldiv;
 
-        $arr = unpack('V', substr($raw, 0, 4));
-        
-        // 色相：値域0～360角度で表す。色相を環状に配置して30分割で使用。
-        // 似通った色が判別しやすいように隣合う色の彩度を変えてある。
-        $h = ($arr[1] & 0x3f)*360/30;
-        $s = ($arr[1] & 0x03) *1; //　彩度：値域0（淡い）～1（濃い)
-        $v = 0.5; // 明度：値域0（暗い）～1（明るい）
-        // 色相　彩度　明度に関しては以下参考の事　http://konicaminolta.jp/instruments/colorknowledge/part1/05.html
+        // HSV（またはHLS）パラメータ設定
+        // レス数が増えるほど、色が濃く、暗くなる
 
-        // 別の、色決定パラメータ
-        //$arr = unpack('V*',substr($id, 0, 8));
-        //$h = floor(($arr[1] % 36)*360/36); // 色相：36分割
-        //$s = ($arr[1] % 3)>=1 ? 0.1 : 0.3; // 彩度：3の剰余が1,2のときは淡く,0のときは少し濃くする
-        //$v =($arr[1] % 3)<=1 ? 1 : 0.8;    // 明度：3の剰余が0,1のときは明るさ最大,2の時はちょっと暗くする
+        // 色相H：値域0～360（角度）
+        $h= $color*360/$coldiv;
+        $h2=$color2*360/$coldiv;
 
-        $hi = floor($h/60) % 6;
-        $f = $h/60-$hi;
-        $p = $v*(1-$s);
-        $q = $v*(1-$f*$s);
-        $t = $v*(1-(1-$f)*$s);
+        $colorMode=2;       // 0:HSV,1:HSL,2:L*C*h
+        switch ($colorMode) {
+        case 0:     // HSV色空間
+            // 彩度S(HSV)：値域0（淡い）～1（濃い)
+            $S=$this->thread->idcount[$id]*0.05;
+            if ($S>1) {$S=1;}
 
-        switch ($hi) {
-            case 0: $R=$v; $G=$t; $B=$p; break;
-            case 1: $R=$q; $G=$v; $B=$p; break;
-            case 2: $R=$p; $G=$v; $B=$t; break;
-            case 3: $R=$p; $G=$q; $B=$v; break;
-            case 4: $R=$t; $G=$p; $B=$v; break;
-            case 5: $R=$v; $G=$p; $B=$q; break;
+            // 明度V(HSV)：値域0（暗い）～1（明るい）
+            $V=1   -$this->thread->idcount[$id]*0.025;
+            if ($L<0.1) {$L=0.1;}
+
+            $color_param=array(
+                array($h,$S,$V,$colorMode), // 背景色（ID本体）
+                array($h2,1,0.6,$colorMode)  // 背景色（ID:部分）
+            );
+            break;
+        case 1:  // HLS色空間
+            // 輝度L(HLS)：値域0（黒）～0.5（純色）～1（白）
+            $L=0.95   -$this->thread->idcount[$id]*0.025;
+            if ($L<0.1) {$L=0.1;}
+
+            // 彩度S(HLS)：値域0（灰色）～1（純色）
+            $S=$this->thread->idcount[$id]*0.05;
+            if ($S>1) {$S=1;}
+
+            $color_param=array(
+                array($h,$L,$S,$colorMode), // 背景色（ID本体）
+                array($h2,0.6,0.5,$colorMode)  // 背景色（ID:部分）
+            );
+            break;
+        case 2:  // L*C*h色空間
+            // 明度L*(L*C*h)：値域0（黒）～50（純色）～100（白）
+            $L=100   -$this->thread->idcount[$id]*2.5;
+            if ($L<10) {$L=10;}
+
+            // 彩度C*(L*C*h)：値域0（灰色）～100（純色）
+            $C=floor(40*sin(deg2rad($this->thread->idcount[$id]*180/50)));
+            if ($C<5) {$C=5;}
+
+            $color_param=array(
+                array($L,$C,$h,$colorMode), // 背景色（ID本体）
+                array(50,60,$h2,$colorMode)  // 背景色（ID:部分）
+            );
+            break;
         }
-        $R = floor($R*255);
-        $G = floor($G*255);
-        $B = floor($B*255);
 
-        $uline = $STYLE['a_underline_none'] == 1 ? '' : "text-decoration:underline";
-        return $idstr = "<span style=\"background-color:rgb({$R},{$G},{$B});{$uline}\">{$idstr}</span>";
+        // 色空間に関する参考資料
+        // HSV,HLS色空間 http://tt.sakura.ne.jp/~hiropon/lecture/color.html
+        // L*C*h表色系 http://konicaminolta.jp/instruments/colorknowledge/part1/08.html
+        // L*a*b*表色系 http://konicaminolta.jp/instruments/colorknowledge/part1/07.html
+        // RGBに変換
+        $rgb=array();
+        for($key=0;$key<count($color_param);$key++) {
+            $colorMode=$color_param[$key][3];
+            if ($colorMode==2) {
+                array_push($rgb,LCh2RGB($color_param[$key]));
+            } else {
+                array_push($rgb,$colorMode 
+                    ? HLS2RGB($color_param[$key])
+                    : HSV2RGB($color_param[$key])
+                );
+                //  unset($color_param[$key]);
+            }
+        }
+
+        // CSSで色をつける
+        $idstr2=preg_split('/:/',$idstr,2); // コロンでID文字列を分割
+        $idstr2[0].=':';
+        $uline=$STYLE['a_underline_none']==1 ? '' : "text-decoration:underline;";
+        $bcolor=array();
+        $LCh=array();
+        for ($i=0;$i<count($rgb);$i++) {
+            if ($rgb['type']=='L*C*h') {
+                $LCh[$i]=$color_param[$i];
+            } else {
+                $LCh[$i]=RGB2LCh($rgb[$i]);
+               /*  if ($LCh[$i][0]<70 && $LCh[$i][0]>40) {
+                  $LCh[$i][0]-=30;
+                  $rgb[$i]=LCh2RGB($LCh[$i]);
+               }*/
+            }
+            $colorcode=$rgb[$i]['color'];
+            $bcolor[$i]="background-color:{$colorcode};";
+            //    $border="border-width:thin;border-style:solid;";
+
+            if      ($LCh[$i][0]>60) {$bcolor[$i].="color:#000;";}
+            else //if ($LCh[$i][0]<40) 
+            {$bcolor[$i].="color:#fff;";}
+        }
+
+        if ($this->thread->idcount[$id]>=25 ) {     // 必死チェッカー発動
+            $uline.="text-decoration:blink;";
+        }
+
+        $idstr2[1]="<span style=\"{$bcolor[0]}{$border}{$uline}\">{$idstr2[1]}</span>";
+        //       $colorprint=1;      // 1にすると、色の変換結果が表示される
+
+        if ($colorprint) {
+            for ($i=0;$i<1;$i++) {
+                switch ($rgb[$i]['type']) {
+                case 'L*C*h' :
+                    $idstr2[1].= "(L*={$rgb[$i][9]},C*={$rgb[$i][10]},h={$rgb[$i][11]})";
+                    $X=$rgb[$i][3];
+                    $Y=$rgb[$i][4];
+                    $Z=$rgb[$i][5];
+                    if ($X>0.9504 || $X<0) {$X="<span style=\"color:#F00\">{$X}</span>";}
+                    if ($Y>1 || $Y<0) {$Y="<span style=\"color:#F00\">{$Y}</span>";}
+                    if ($Z>1.0889 || $Z<0) {$Z="<span style=\"color:#F00\">{$Z}</span>";}
+                    $idstr2[1].= ",(X={$X},Y={$Y},Z={$Z})";
+
+                    break;
+                case 'HSV' :$idstr2[1].= "(H={$rgb[$i][3]},S={$rgb[$i][4]},V={$rgb[$i][5]})";
+                    break;
+                case 'HLS' :$idstr2[1].= "(H={$rgb[$i][3]},L={$rgb[$i][4]},S={$rgb[$i][5]})";
+                    break;
+                }
+
+                $R=$rgb[$i][0];
+                $G=$rgb[$i][1];
+                $B=$rgb[$i][2];
+                if ($R>255 || $R<0) {$R="<span style=\"color:#F00\">{$R}</span>";}
+                if ($G>255 || $G<0) {$G="<span style=\"color:#F00\">{$G}</span>";}
+                if ($B>255 || $B<0) {$B="<span style=\"color:#F00\">{$B}</span>";}
+                $idstr2[1].= ",(R={$R},G={$G},B={$B}),{$rgb[$i]['color']}";
+            }
+            //  $idstr2[1].= join(",",$rgb[0]);
+        }
+
+        if (isset($rgb[1])) {
+            $idstr2[0]="<span style=\"{$bcolor[1]}{$border}{$uline}\">{$idstr2[0]}</span>";
+        }
+        $idstr=join('',$idstr2);
+        $this->id2idstr[$id]=$idstr;
+        return $idstr;
     }
 
     // }}}
