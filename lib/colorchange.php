798171e84e7a870558e472c0942867fad9782193
unpush <unpush@gmail.com>
2009-07-18 19:40:44 +0900

    colorchange.php バグ修正(typo?)

--- lib/colorchange.php
+++ lib/colorchange.php
@@ -15,7 +15,7 @@ function RGB2ColorCode ($r,$g,$b) {
     if ($r<0) {$r=0;}
     if ($g>255) {$g=255;}
     if ($g<0) {$g=0;}
-    if ($br>255) {$b=255;}
+    if ($b>255) {$b=255;}
     if ($b<0) {$b=0;}
 
     return "#".
