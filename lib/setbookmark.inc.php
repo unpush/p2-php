60f3920f8058a044ea54c9bae6598124cff193d8
rsk <rsk@a8256b1f-a225-0410-95af-91ab0afbf006>
2009-09-11 18:28:21 +0000

    expack: - 新機能(予定)のためのファイルを追加。ただし未実装。 - NGあぼーんの修正・リファクタリング。
    git-svn-id: http://svn.sourceforge.jp/svnroot/p2-php/p2ex/trunk@726 a8256b1f-a225-0410-95af-91ab0afbf006

---
+++ lib/setbookmark.inc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * rep2expack - ブックマーク登録・解除
+ */
+
+require_once P2_LIB_DIR . '/FileCtl.php';
+
+// {{{ setbookmark()
+
+/**
+ * レスをブックマークにセットする
+ *
+ * @param   string  $host
+ * @param   string  $bbs
+ * @param   int     $key
+ * @param   int     $resnum
+ * @param   bool    $set
+ * @param   string  $ttitle
+ * @param   string  $memo
+ * @return  bool
+ */
+function setbookmark($host, $bbs, $key, $resnum, $set, $ttitle = null, $memo = null)
+{
+    return false;
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: cp932
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+// vim: set syn=php fenc=cp932 ai et ts=4 sw=4 sts=4 fdm=marker:
